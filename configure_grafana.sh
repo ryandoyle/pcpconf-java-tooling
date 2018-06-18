81c94befcffcf94d1edb28ec35274641bf383fee
Ryan Doyle <ryan@doylenet.net>
2018-06-18 19:54:17 +1000

    Use proxy mode

--- configure_grafana.sh
+++ configure_grafana.sh
@@ -2,8 +2,8 @@
 curl -X POST -H "Content-Type: application/json" http://admin:secret@localhost:3000/api/datasources -d '{
   "name":"graphite",
   "type":"graphite",
-  "url":"http://localhost:8088",
-  "access":"direct",
+  "url":"http://graphite:80",
+  "access":"proxy",
   "isDefault" : true,
   "basicAuth":false
 }'
